8fe9997ecde46ac5b14f6fa24b28216a117e9fd2
Adrien du Repaire <adrien@danim.com>
2020-11-13 11:19:43 +0100

    win

---
+++ bin/postinstall.js
@@ -0,0 +1,5 @@
+const sys = require('sys');
+const exec = require('child_process').exec;
+
+const archive = `esdebugger-core-${process.platform === 'darwin' ? 'mac' : 'win'}.tar.gz`;
+exec(`tar -xzf ${archive}`, (error, stdout, stderr) => sys.puts(stdout)));
